441734314892e79de1d9e2889dacd323da84904f
iki <drymtn721@gmail.com>
2022-11-02 17:29:06 +0900

    1102x

--- dev/front/src/components/Table.tsx
+++ dev/front/src/components/Table.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataGrid, GridColDef, GridToolbar, jaJP } from '@mui/x-data-grid';
 import { useDemoData } from '@mui/x-data-grid-generator';
 import axios from 'axios';
 
-function TestJsonData() {
+
+
+function Table() {
   const [post, setPosts] = useState([])
 
   useEffect(() => {
@@ -12,28 +14,37 @@ function TestJsonData() {
       setPosts(res.data)
     })
   }, [])
+
+  console.log("=====================-");
+  console.log(Object.keys(post)[0]);
+  // console.log();
+
   const getTableContent = getObj => {
-    const bodyTableContent = {...getObj};
     return (
-      <div>{bodyTableContent}</div>
-    )
+      <div style={{ padding: "3vh", margin: "auto", height: "82vh", width: '95%', userSelect: "None" }}>
+        <DataGrid columns={data.columns as GridColumns} rows={post} density="standard" localeText={jaJP.components.MuiDataGrid.defaultProps.localeText} hideFooter/>
+      </div>
+    );
   }
 
 
 }
 
-const Table: React.FC = () => {
-  const { data } = useDemoData({
-    dataSet: 'Commodity',
-    rowLength: 10000,
-    editable: true,
-  });
 
-  return (
-    <div style={{ padding: "3vh", margin: "auto", height: "82vh", width: '95%', userSelect: "None" }}>
-      <DataGrid columns={data.columns as GridColumns} rows={data.rows} density="standard" localeText={jaJP.components.MuiDataGrid.defaultProps.localeText} hideFooter/>
-    </div>
-  );
-};
+
+
+// const Table: React.FC = () => {
+//   const { data } = useDemoData({
+//     dataSet: 'Commodity',
+//     rowLength: 10000,
+//     editable: true,
+//   });
+
+//   return (
+//     <div style={{ padding: "3vh", margin: "auto", height: "82vh", width: '95%', userSelect: "None" }}>
+//       <DataGrid columns={data.columns as GridColumns} rows={data.rows} density="standard" localeText={jaJP.components.MuiDataGrid.defaultProps.localeText} hideFooter/>
+//     </div>
+//   );
+// };
 
 export default Table;
