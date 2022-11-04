0dd573dab751d274b8c299d6cd9cffcfb009e644
iki <drymtn721@gmail.com>
2022-11-04 17:29:59 +0900

    1104

--- dev/back/back_end/settings.py
+++ dev/back/back_end/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'back_end.urls'
@@ -123,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173',#React用
+    'http://localhost:5173',#React用
+]
--- dev/front/src/components/Table.tsx
+++ dev/front/src/components/Table.tsx
@@ -5,46 +5,69 @@ import axios from 'axios';
 
 
 
-function Table() {
-  const [post, setPosts] = useState([])
-
-  useEffect(() => {
-    axios.get('http://jsonplaceholder.typicode.com/posts')
-    .then(res => {
-      setPosts(res.data)
-    })
-  }, [])
-
-  console.log("=====================-");
-  console.log(Object.keys(post)[0]);
-  // console.log();
-
-  const getTableContent = getObj => {
-    return (
-      <div style={{ padding: "3vh", margin: "auto", height: "82vh", width: '95%', userSelect: "None" }}>
-        <DataGrid columns={data.columns as GridColumns} rows={post} density="standard" localeText={jaJP.components.MuiDataGrid.defaultProps.localeText} hideFooter/>
-      </div>
-    );
-  }
-
-
-}
-
-
-
-
 // const Table: React.FC = () => {
-//   const { data } = useDemoData({
-//     dataSet: 'Commodity',
-//     rowLength: 10000,
-//     editable: true,
-//   });
+  
 
+//   const [post, setPosts] = useState([])
+
+//   useEffect(() => {
+//     // axios.get('http://jsonplaceholder.typicode.com/posts')
+//     axios.get('http://127.0.0.1:8000/api/posts/')
+//     .then(res => {
+//       setPosts(res.data)
+//     })
+//   }, [])
+
+//   const testColumns = [
+//     {field: "id", headerName: "ID", editable: true},
+//     {field: "title", headerName: "TITLE", editable: true},
+//     {field: "body", headerName: "BODY", editable: true},
+//   ];
+//   const testRows = [
+//     {id: "test-id1", title: "test-title1", body: "test-body1"},
+//     {id: "test-id2", title: "test-title2", body: "test-body2"},
+//   ]
+
+//   console.log(post);
+  
 //   return (
 //     <div style={{ padding: "3vh", margin: "auto", height: "82vh", width: '95%', userSelect: "None" }}>
-//       <DataGrid columns={data.columns as GridColumns} rows={data.rows} density="standard" localeText={jaJP.components.MuiDataGrid.defaultProps.localeText} hideFooter/>
+//       <DataGrid columns={testColumns as GridColumns} rows={post} density="standard" localeText={jaJP.components.MuiDataGrid.defaultProps.localeText} hideFooter/>
 //     </div>
 //   );
 // };
+
+
+
+const Table: React.FC = () => {
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 100,
+    editable: true,
+  });
+  
+
+  const testColumns = [
+    {field: "id", headerName: "ID"},
+    {field: "title", headerName: "TITLE"},
+    {field: "body", headerName: "BODY"},
+  ];
+  const testRows = [
+    {id: "test-id1", title: "test-title1", body: "test-body1"},
+    {id: "test-id2", title: "test-title2", body: "test-body2"},
+  ]
+
+  console.log( data.columns );
+
+
+  return (
+    <div style={{ padding: "3vh", margin: "auto", height: "82vh", width: '95%', userSelect: "None" }}>
+      <DataGrid columns={data.columns as GridColumns} rows={data.rows} density="standard" localeText={jaJP.components.MuiDataGrid.defaultProps.localeText} hideFooter/>
+    </div>
+  );
+};
+
+
+
 
 export default Table;
